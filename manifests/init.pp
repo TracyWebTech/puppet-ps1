dbe931294cd5c13304c91a0c2e2d1902f81da9b0
Sergio Oliveira <sergio@tracy.com.br>
2012-11-07 01:51:23 -0200

    Using variables and unless for ps1 commands

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,21 +8,30 @@ class ps1 (
     default => '1'
   }
 
+  $force_color_prompt_cmd = "/bin/sed -i -re '/force_color_prompt=yes/ s/^#//'"
+  $force_color_prompt_condition = 'grep "#force_color_prompt"'
+
+  $ps1_color_cmd = "/bin/sed -i -re 's/01;3[1-3]m/01;3${color_code}m/'"
+  $ps1_color_condition = "grep '01;3${color_code}m'"
+
+  $skel_path = '/etc/skel/.bashrc'
+  $root_path = '/root/.bashrc'
+
   # uncomment force_color_prompt in skel
-  exec {
-    "/bin/sed -i -re '/force_color_prompt=yes/ s/^#//' /etc/skel/.bashrc":
+  exec { "$force_color_prompt_cmd $skel_path":
+    onlyif => "$force_color_prompt_condition $skel_path"
   }
   # uncomment  force_color_prompt in root
-  exec {
-    "/bin/sed -i -re '/force_color_prompt=yes/ s/^#//' /root/.bashrc":
+  exec { "$force_color_prompt_cmd $root_path":
+    onlyif => "$force_color_prompt_condition $root_path"
   }
 
   # change prompt color in skel
-  exec {
-    "/bin/sed -i -re 's/01;3[1-3]m/01;3${color_code}m/' /etc/skel/.bashrc":
+  exec { "$ps1_color_cmd $skel_path":
+    unless => "$ps1_color_condition $skel_path",
   }
   # change prompt color in root
-  exec {
-    "/bin/sed -i -re 's/01;3[1-3]m/01;3${color_code}m/' /root/.bashrc":
+  exec { "$ps1_color_cmd $root_path":
+    unless => "$ps1_color_condition $root_path",
   }
 }
