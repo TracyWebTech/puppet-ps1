d94c86d4754144a6b6f0e03bf124d0f8c65e6a00
Sergio Oliveira <seocam@seocam.com>
2016-02-16 19:38:26 -0200

    Set PS1 on bash_completion

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,30 +8,27 @@ class ps1 (
     default => '1'
   }
 
-  $force_color_prompt_cmd = "/bin/sed -i -re '/force_color_prompt=yes/ s/^#//'"
-  $force_color_prompt_condition = '/bin/grep "#force_color_prompt"'
+  if $::operatingsystem == 'ubuntu' {
+    $root_path = '/root/.bashrc'
 
-  $ps1_color_cmd = "/bin/sed -i -re 's/01;3[1-3]m/01;3${color_code}m/'"
-  $ps1_color_condition = "/bin/grep '01;3${color_code}m'"
+    # uncomment  force_color_prompt in root
+    $force_color_prompt_cmd = "/bin/sed -i -re '/force_color_prompt=yes/ s/^#//'"
+    $force_color_prompt_condition = '/bin/grep "#force_color_prompt"'
 
-  $skel_path = '/etc/skel/.bashrc'
-  $root_path = '/root/.bashrc'
+    exec { "$force_color_prompt_cmd $root_path":
+      onlyif => "$force_color_prompt_condition $root_path"
+    }
 
-  # uncomment force_color_prompt in skel
-  exec { "$force_color_prompt_cmd $skel_path":
-    onlyif => "$force_color_prompt_condition $skel_path"
-  }
-  # uncomment  force_color_prompt in root
-  exec { "$force_color_prompt_cmd $root_path":
-    onlyif => "$force_color_prompt_condition $root_path"
-  }
+    # change prompt color in root
+    $ps1_color_cmd = "/bin/sed -i -re 's/01;3[1-3]m/01;3${color_code}m/'"
+    $ps1_color_condition = "/bin/grep '01;3${color_code}m'"
 
-  # change prompt color in skel
-  exec { "$ps1_color_cmd $skel_path":
-    unless => "$ps1_color_condition $skel_path",
-  }
-  # change prompt color in root
-  exec { "$ps1_color_cmd $root_path":
-    unless => "$ps1_color_condition $root_path",
+    exec { "$ps1_color_cmd $root_path":
+      unless => "$ps1_color_condition $root_path",
+    }
+
+    file { '/etc/bash_completion.d/ps1':
+      content => "PS1='${debian_chroot:+($debian_chroot)}\[\033[01;3${color_code}m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '",
+    }
   }
 }
